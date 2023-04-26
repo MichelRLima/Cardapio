35798a64a47f2b27e0d97fac94eaec0b61f99b23
MichelRLima <michelrocha502@gmail.com>
2023-04-26 19:22:05 -0300

    first commit

--- src/Titulo.js
+++ src/Titulo.js
@@ -3,7 +3,7 @@ function Titulo (){
 
     return(
         <>
-        <div className="titulo" style={{backgroundColor: "white", color:"red", width: "80%", margin: "0 auto", borderRadius:"15px", border: "1px solid red", textAlign:"center"}}>
+        <div className="titulo" style={{position:"absolute", bottom:"0",backgroundColor: "white", color:"red", width: "100%", margin: "50px auto", borderRadius:"15px", border: "1px solid red", textAlign:"center"}}>
     
         <p>Criado por <a target="_blanck" href="https://www.linkedin.com/in/michel-rocha-01b550210/">Michel Rocha</a> em React</p>
       </div>
